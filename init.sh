c3b8f85b8667595e0568753b6d5d95bf9d48c407
Peter Rauscher <peter@peterrauscher.com>
2025-06-21 15:56:18 -0400

    Fix init script

--- init.sh
+++ init.sh
@@ -4,4 +4,4 @@
 sudo mkdir -p /var/www/api.peterrauscher.com/.well-known
 sudo ln -sf $HOME/api.peterrauscher.com/openapi.json /var/www/api.peterrauscher.com/openapi.json
 sudo ln -sf $HOME/api.peterrauscher.com/.well-known/mcp.json /var/www/api.peterrauscher.com/.well-known/mcp.json
-sudo ln -sf $HOME/api.peterrauscher.com/nginx.conf/etc/nginx/sites-enabled/api.peterrauscher.com
+sudo ln -sf $HOME/api.peterrauscher.com/nginx.conf /etc/nginx/sites-enabled/api.peterrauscher.com
